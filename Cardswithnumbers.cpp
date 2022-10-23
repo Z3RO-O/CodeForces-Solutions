32e835f94a4996eabbda10b807007676dc430e2b
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2022-10-23 19:08:46 +0530

    solution

---
+++ Cardswithnumbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+    vector<int> v;
+    cin>>n;
+    for (int i = 0; i < 2*n; i++)
+    {
+        
+    }
+    
+    return 0;
+}
